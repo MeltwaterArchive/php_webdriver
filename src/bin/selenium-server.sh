ecd70bd1a3e7a3bcf4bc6dc98c79a39c77ad3032
Stuart Herbert <stuart@stuartherbert.com>
2013-08-13 19:48:39 +0100

    Fix: do not rely on screen to kill all child processes

--- src/bin/selenium-server.sh
+++ src/bin/selenium-server.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 #
 # selenium-server.sh
 #	start | stop selenium-server.sh
@@ -69,6 +69,9 @@ function stop() {
 		return 0
 	fi
 
+	# stop the children first
+	kill_children $pid
+
 	kill $pid
 	pid=`get_pid`
 	if [[ -n $pid ]] ; then
@@ -132,6 +135,15 @@ function get_pid() {
 	if [[ -n $pid ]] ; then
 		echo "$pid"
 	fi
+}
+
+function kill_children() {
+	local ppid=$1
+
+	# get the pid of all the child processes
+	for x in `ps -ef | awk '$3 == '${ppid}' { print $2 }'` ; do
+		kill $x
+	done
 }
 
 case "$1" in
